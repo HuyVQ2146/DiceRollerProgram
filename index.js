f164bf989a77fd362adad3e3312bd0330d6c2c4b
Vu Quang Huy <Huy.VQ2421184@sis.hust.edu.vn>
2025-02-22 07:20:07 +0700

    tài xỉu

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function rollDice(){
     const num = document.getElementById('num').value;
-    const res = document.getElementById('res');
+    const p1 = document.getElementById('p1');
+    const p2 = document.getElementById('p2');
     const img = document.getElementById('img');
     const values = [];
     const images = [];
@@ -10,14 +11,24 @@ function rollDice(){
         return;
     }
     
+    let count = 0;
     for (let i = 0; i < num; i++) {
         const value  = Math.floor(Math.random() * 6) +1;
         values.push(value);
         images.push(`<img src="dice/dice${value}.jpg" alt="Dice ${value}" >`);
             // cái này có thể push cả img bằng lệnh trong html (phải dùng ``)
             // và trong `` vx có thể dùng ${} để format
+        count += value;
+    };
+    console.log(count); 
+
+    p1.textContent = `Dice: ${values.join(', ')}` + ".";
+    if (count > 10) {
+        p2.textContent = `Tài!`;
+    }
+    else {
+        p2.textContent = `Xỉu!`;
     };
 
-    res.textContent = `Dice: ${values.join(', ')}`;
     img.innerHTML = images.join('');
 }
